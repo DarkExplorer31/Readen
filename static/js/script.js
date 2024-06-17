1e475e3352f59e787f45f0abaad85af826195fc6
DarkExplorer31 <drkexplorer@gmal.com>
2024-06-17 15:20:10 +0200

    Add fonction nextPage in JS to turn pages with pdf

--- static/js/script.js
+++ static/js/script.js
@@ -73,8 +73,6 @@ async function initialize() {
             await fetchAndSetAudio(audioPlayer, audioPlayer.dataset.audioUrl);
             setupPlaybackControls(audioPlayer, backwardButton, forwardButton);
             setupVideoSync(audioPlayer, readingVideo);
-        } else {
-            console.error('Elements not found on read_corner page.');
         }
     }
 }
@@ -109,82 +107,222 @@ closeMenu.addEventListener('click', hideMenu);
 menuOverlay.addEventListener('click', hideMenu);
 
 /* Book Animation*/
+const container = document.querySelector("#book-container");
 const prevBtn = document.querySelector("#prev-btn");
 const nextBtn = document.querySelector("#next-btn");
 const book = document.querySelector("#book");
 
-const paper1 = document.querySelector("#p1");
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.11.338/pdf.worker.min.js';
+
+const pdfPath = book.getAttribute('data-pdf');
+const fullPDFPath = '/media/' + pdfPath;
+let numOfPapers = 0;
+
+function getPDFPageCount(fullPDFPath, callback) {
+    pdfjsLib.getDocument(fullPDFPath).promise.then(pdf => {
+        callback(pdf.numPages);
+    }).catch(error => {
+        console.error('Error: ' + error.message);
+        callback(null);
+    });
+}
+
+getPDFPageCount(fullPDFPath, function (numPages) {
+    if (numPages !== null) {
+        numOfPapers = numPages;
+        console.log('Number of pages:', numOfPapers);
+        maxLocation = numOfPapers + 4;
+    } else {
+        console.log('Failed to load the PDF.');
+    }
+});
+
+function renderPDFPageToImage(pdfDoc, pageNumber, container) {
+    pdfDoc.getPage(pageNumber).then(page => {
+        const viewport = page.getViewport({ scale: 1.0 });
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        const renderContext = {
+            canvasContext: context,
+            viewport: viewport
+        };
+        page.render(renderContext).promise.then(() => {
+            const img = document.createElement('img');
+            img.src = canvas.toDataURL();
+            container.innerHTML = '';
+            container.appendChild(img);
+        });
+    });
+}
+
+function loadPDFPage(pdfPath, pageNumber, container) {
+    pdfjsLib.getDocument(pdfPath).promise.then(pdfDoc => {
+        renderPDFPageToImage(pdfDoc, pageNumber, container);
+    }).catch(error => {
+        console.error('Error: ' + error.message);
+    });
+}
+
+const firstPaper = document.querySelector("#first");
 const paper2 = document.querySelector("#p2");
 const paper3 = document.querySelector("#p3");
+const lastPaper = document.querySelector("#last");
 
-// Event Listener about Book
-prevBtn.addEventListener("click", goPrevPage);
 nextBtn.addEventListener("click", goNextPage);
 
-let currentLocation = 1;
-let numOfPapers = 3;
-let maxLocation = numOfPapers + 1;
+function exitZoom() {
+    prevBtn.classList.remove("open");
+    nextBtn.classList.remove("open");
+    container.classList.remove("book-open");
+    book.classList.remove("open");
+}
 
 function openBook() {
+    container.classList.add("book-open");
+    book.classList.add("open");
+    prevBtn.classList.add("open");
+    nextBtn.classList.add("open");
+    firstPaper.classList.add("flipped");
+    firstPaper.style.zIndex = 1;
     book.style.transform = "translateX(50%)";
-    prevBtn.style.transform = "translateX(-180px)";
-    nextBtn.style.transform = "translateX(180px)";
+    prevBtn.style.transform = "translateX(-330px)";
+    nextBtn.style.display = "block";
+    prevBtn.style.display = "block";
+    nextBtn.style.transform = "translateX(330px)";
 }
 
 function closeBook(isAtBeginning) {
     if (isAtBeginning) {
         book.style.transform = "translateX(0%)";
+        prevBtn.style.display = "none";
+        paper2.classList.remove("flipped");
+        paper3.classList.remove("flipped");
+        lastPaper.classList.remove("flipped");
     } else {
+        paper2.classList.add("flipped");
+        paper3.classList.add("flipped");
+        lastPaper.classList.add("flipped");
+        lastPaper.style.zIndex = 4;
         book.style.transform = "translateX(100%)";
+        nextBtn.style.display = "none";
     }
+    exitZoom();
     prevBtn.style.transform = "translateX(0px)";
     nextBtn.style.transform = "translateX(0px)";
 }
 
-function goNextPage() {
-    if (currentLocation < maxLocation) {
-        switch (currentLocation) {
-            case 1:
-                openBook();
-                paper1.classList.add("flipped");
-                paper1.style.zIndex = 1;
-                break;
-            case 2:
-                paper2.classList.add("flipped");
-                paper2.style.zIndex = 2;
-                break;
-            case 3:
-                paper3.classList.add("flipped");
-                paper3.style.zIndex = 3;
-                closeBook();
-                break;
-            default:
-                throw new Error("unknow state");
-        }
-        currentLocation++;
+let pointOfView = 1;
+let view = 1;
+let p1Position = 1;
+let p2Position = 2;
+let p3Position = 3;
+let p4Position = 4;
+
+function goNextPageUnderFivePage() {
+    if (view === 1) {
+        openBook();
+        loadPDFPage(fullPDFPath, p1Position, paper2.querySelector('.front-content'));
+        view += 1;
+    } else if (view === 2 && numOfPapers === 1) {
+        closeBook();
+    } else if (view === 2 && numOfPapers === 2) {
+        loadPDFPage(fullPDFPath, p2Position, paper2.querySelector('.back-content'));
+        paper2.classList.add("flipped");
+        paper3.classList.add("flipped");
+        view += 1;
+    } else if (view === 3 && numOfPapers === 2) {
+        closeBook();
+    } else if (view === 2 && numOfPapers > 2) {
+        loadPDFPage(fullPDFPath, p2Position, paper2.querySelector('.back-content'));
+        loadPDFPage(fullPDFPath, p3Position, paper3.querySelector('.front-content'));
+        paper2.classList.add("flipped");
+        view += 1;
+    } else if (view === 3 && numOfPapers === 3) {
+        closeBook();
+    } else if (view === 3 && numOfPapers === 4) {
+        loadPDFPage(fullPDFPath, p4Position, paper3.querySelector('.back-content'));
+        paper3.classList.add("flipped");
+        paper2.style.zIndex = 2;
+        paper3.style.zIndex = 3;
+        view += 1;
+    } else {
+        closeBook();
     }
 }
 
-function goPrevPage() {
-    if (currentLocation > 1) {
-        switch (currentLocation) {
-            case 2:
-                closeBook(true);
-                paper1.classList.remove("flipped");
-                paper1.style.zIndex = 3;
-                break;
-            case 3:
-                paper2.classList.remove("flipped");
+function goNextPage() {
+    if (pointOfView < 4 && numOfPapers > 4) {
+        if (pointOfView === 1) {
+            pointOfView += 1;
+            openBook();
+            loadPDFPage(fullPDFPath, p1Position, paper2.querySelector('.front-content'));
+        } else if (pointOfView === 2) {
+            if (p1Position === numOfPapers || p3Position === numOfPapers) {
+                pointOfView += 1;
+            }
+            if (p2Position === numOfPapers) {
+                loadPDFPage(fullPDFPath, p2Position, paper2.querySelector('.back-content'));
+                paper2.classList.add("flipped");
+                paper3.classList.add("flipped");
+                paper2.style.zIndex = 3;
+                paper3.style.zIndex = 2;
+                paper2.querySelector(".front").style.transition = "transform 0.8s";
+                paper2.querySelector(".back").style.transition = "transform 0.8s";
+                pointOfView += 1;
+            } else if (p4Position === numOfPapers) {
+                loadPDFPage(fullPDFPath, p4Position, paper3.querySelector('.back-content'));
+                paper2.classList.add("flipped");
+                paper3.classList.add("flipped");
+                paper3.querySelector(".front").style.transition = "transform 0.8s";
+                paper3.querySelector(".back").style.transition = "transform 0.8s";
                 paper2.style.zIndex = 2;
-                break;
-            case 4:
-                openBook();
+                paper3.style.zIndex = 3;
+                pointOfView += 1;
+            } else if (view === 2) {
+                firstPaper.style.zIndex = 1;
+                p1Position += 4;
+                paper2.classList.add("flipped");
+                loadPDFPage(fullPDFPath, p2Position, firstPaper.querySelector('.back-content'));
+                loadPDFPage(fullPDFPath, p2Position, paper2.querySelector('.back-content'));
+                loadPDFPage(fullPDFPath, p3Position, paper3.querySelector('.front-content'));
+                loadPDFPage(fullPDFPath, p4Position, paper3.querySelector('.back-content'));
+            } else if (view % 2 !== 0 && view !== 1) {
+                p2Position += 4;
+                p3Position += 4;
+                loadPDFPage(fullPDFPath, p4Position, paper3.querySelector('.back-content'));
+                loadPDFPage(fullPDFPath, p3Position, paper3.querySelector('.front-content'));
+                loadPDFPage(fullPDFPath, p4Position, firstPaper.querySelector('.back-content'));
+                loadPDFPage(fullPDFPath, p1Position, paper2.querySelector('.front-content'));
+                paper3.classList.add("flipped");
+                paper2.classList.remove("flipped");
+                paper3.querySelector(".front").style.transition = "transform 0.8s";
+                paper3.querySelector(".back").style.transition = "transform 0.8s";
+                paper2.querySelector(".front").style.transition = "transform 0s";
+                paper2.querySelector(".back").style.transition = "transform 0s";
+            } else if (view % 2 === 0 && view !== 2) {
+                p1Position += 4;
+                p4Position += 4;
+                loadPDFPage(fullPDFPath, p2Position, paper2.querySelector('.back-content'));
+                loadPDFPage(fullPDFPath, p1Position, paper2.querySelector('.front-content'));
+                loadPDFPage(fullPDFPath, p2Position, firstPaper.querySelector('.back-content'));
+                loadPDFPage(fullPDFPath, p3Position, paper3.querySelector('.front-content'));
+                paper2.classList.add("flipped");
                 paper3.classList.remove("flipped");
-                paper3.style.zIndex = 1;
-                break;
-            default:
-                throw new Error("unknow state");
+                paper3.querySelector(".front").style.transition = "transform 0s";
+                paper3.querySelector(".back").style.transition = "transform 0s";
+                paper2.querySelector(".front").style.transition = "transform 0.8s";
+                paper2.querySelector(".back").style.transition = "transform 0.8s";
+            }
+        } else if (pointOfView === 3) {
+            closeBook();
+            return;
         }
-        currentLocation--;
+        view += 1;
+        console.log(`view: ${view}, p1: ${p1Position}, p2: ${p2Position}, p3: ${p3Position}, p4: ${p4Position}, numOfPaper: ${numOfPapers}, pointOfView: ${pointOfView}`)
+    } else if (numOfPapers <= 4) {
+        goNextPageUnderFivePage();
     }
 }
